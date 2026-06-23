a9a0dba5da551ec5b82726bda4f3cb06505c90ca
Sonya <94686155+slooonya@users.noreply.github.com>
2026-06-23 16:34:24 +0800

    chore: add starting files

---
+++ src/test/java/com/slooonya/pageturner/PageTurnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slooonya.pageturner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageTurnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
